6bc54038a539a7c9e4b4bae0d249c88d67c9aee4
rkhuda <rkhuda@stuy.edu>
2017-11-29 19:45:04 -0500

    adding abstract class LibraryBook

---
+++ 05Library/LibraryBook.java
@@ -0,0 +1,29 @@
+public abstract class LibraryBook extends Book implements Comparable<LibraryBook> {
+    
+    private String callNumber;
+
+    public LibraryBook(String author, String title, String ISBN, String callNumber){
+	super(author, title, ISBN);
+	this.callNumber = callNumber;
+    }
+
+    public String getCallNumber(){
+	return callNumber;
+    }
+    public void setCallNumber(String callNumber){
+	this.callNumber = callNumber;
+    }
+
+    public abstract void checkout(String patron, String due);
+    public abstract void returned();
+    public abstract String circulationStatus();
+    
+    public int compareTo(LibraryBook other){
+	return getCallNumber().compareTo(other.getCallNumber());
+    }
+
+    public String toString(){
+	return getTitle() + ", " + getAuthor() + ", " + getISBN() + ", " + circulationStatus() + ", "+ getCallNumber();
+    }
+    
+}
