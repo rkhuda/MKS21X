a8fa81386ce664bfcbacb3c00dd1dee2bdb4215d
rkhuda <rkhuda@stuy.edu>
2017-12-13 18:10:01 -0500

    fixed and tested code

--- 09GUI/ConvertTemp.java
+++ 09GUI/ConvertTemp.java
@@ -1,52 +1,64 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
 public class ConvertTemp extends JFrame implements ActionListener{
     private Container pane;
-
-    private JButton b, b2;
+    private JButton b;
+    private JCheckBox c,c2;
     private JTextField t;
 
-    public ConvertTemp() {
-	this.setTitle("Temperature Converson");
-	this.setSize(600, 400);
-	this.setLocation(100, 100);
-	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-	pane = this.getContentPane();
-	pane.setLayout(new FlowLayout());
-
-	t = new JTextField(12);
-	b = new JButton("Farenheit");
-	b2 = new JButton("Celsius");
-
-	pane.add(t);
-	pane.add(b);
-	pane.add(b2);
-    }
-
-    public static void main(String[] args){
-	ConvertTemp g = new ConvertTemp();
-	g.setVisible(true);
-    }
-
-    public static double farenheitToCelsius(double f){
-	return ((f - 32) / 1.8);
-    }
-
-    public static double celsiusToFarenheit(double c){
-	return ((c*1.8) + 32);
-    }
-
     public void actionPerformed(ActionEvent e){
-	String s = e.getActionCommand();
-	System.out.println(s);
-	if (s.equals("Farenheit")){
-	    t.setText("" + celsiusToFarenheit(Double.parseDouble(t.getText())));
-	}
-	if (s.equals("Celsius")){
-	    t.setText("" + farenheitToCelsius(Double.parseDouble(t.getText())));
-	}   
+        String s = e.getActionCommand();
+        if(s.equals("Convert")){
+            if( c.isSelected() ){
+		System.out.println(
+				   "" + CelsiusToFarenheit(Double.parseDouble(t.getText()))
+				   );
+            }
+	    else if (c2.isSelected()){
+		System.out.println(
+				   "" + FarenheitToCelsius(Double.parseDouble(t.getText()))
+				   );
+            }
+        }
+
+    }
+
+    public ConvertTemp() {
+        this.setTitle("Temperature Conversion");
+        this.setSize(500,80);
+        this.setLocation(100,100);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        pane = this.getContentPane();
+        pane.setLayout(new FlowLayout());
+        b = new JButton("Convert");
+        c = new JCheckBox("Farenheit");
+	c2 = new JCheckBox("Celsius");
+        t = new JTextField(15);
+
+        b.addActionListener(this);
+        t.addActionListener(this);
+        c.addActionListener(this);
+	c2.addActionListener(this);
+
+        pane.add(t);
+        pane.add(c);
+        pane.add(c2);
+        pane.add(b);
+
+    }
+
+    public static double FarenheitToCelsius(double F) {
+	return ((F - 32) / 1.8);
+    }
+
+    public static double CelsiusToFarenheit(double C) {
+	return ((C*1.8) + 32);
+    }
+
+    public static void main(String[] args) {
+        ConvertTemp g = new ConvertTemp();
+        g.setVisible(true);
     }
 }
